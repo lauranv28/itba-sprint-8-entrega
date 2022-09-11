9639742374256fbc2d5d6fdcc6c1c4ee077e5973
Laura Vargas <vargasnlaura@gmail.com>
2022-09-10 22:38:37 -0300

    Saco serializers

--- homebanking/cuentas/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Cuentas
-from django.contrib.auth.models import User
-
-class CuentaSerializer(serializers.HyperlinkedModelSerializer):
-    cuenta = serializers.HyperlinkedRelatedField(many=True, view_name='cuentas/cuentas.html', read_only=True)
-    class Meta:
-        model = Cuenta
-        fields = ['balance', 'tipos_de_cuentas_id']
